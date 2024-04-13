82155ddb36e04b7f27e91b764fc9a9f9febbc1c4
Michael Specht <micha.specht@gmail.com>
2024-04-13 09:02:08 +0200

    yay

--- convert-to-si.rb
+++ convert-to-si.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
 
-Dir['scripts/cache/svg-scour/*.svg'].each_slice(500) { |x| system("dart run jovial_svg:svg_to_si -b -o scripts/ca
-che/svg-scour-jovial-32/ #{x.join(' ')}") }
+require 'fileutils'
+
+FileUtils.mkpath('scripts/cache/svg-scour-jovial')
+Dir['scripts/cache/svg-scour/*.svg'].each_slice(500) { |x| system("dart run jovial_svg:svg_to_si --no-big -o scripts/cache/svg-scour-jovial/ #{x.join(' ')}") }
